443e1b414ef6b3564b42a60944d0969cabbc35e1
oga8867 <30932394+oga8867@users.noreply.github.com>
2023-04-11 12:00:11 +0900

    Update research.py

--- research.py
+++ research.py
@@ -1,4 +1,3 @@
-#streamlit run research.py
 import streamlit as st
 import pandas as pd
 import requests
@@ -6,7 +5,13 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import time
 import numpy as np
-global x, char_lv, charname, charclass, data
+import base64
+from io import BytesIO
+import zipfile
+
+
+
+# global x, char_lv, charname, charclass, data
 data = []
 API_KEY = "eEWia7fBRvOd4sws6g7rgL9wqyUcKXEH"
 def main():
@@ -20,6 +25,7 @@ def main():
     name = st.text_input('당신 캐릭터의 이름은?')
     charservername=servers[SERVER_NAME]
     try:
+        global x, char_lv, charname, charclass, data
         if st.button('검색'):
 
             print(servers[SERVER_NAME])
@@ -36,32 +42,33 @@ def main():
                 print(char_lv)
             st.write(x)
 
-
-
-
     except:
         st.write("캐릭터가 존재하지 않는것 같아요.")
         st.image('./error.gif')
 
 
     try:
+
+
+        url = f"https://api.neople.co.kr/df/servers/{servers[SERVER_NAME]}/characters?characterName={name}&apikey={API_KEY}"
+        response = requests.get(url)
+        player_info = response.json()
+        char_lv = player_info['rows'][0]['level']
+        charname = player_info['rows'][0]['characterName']
+        charclass = player_info['rows'][0]['jobName']
+
         if char_lv >= 110:
             examples = '만렙이시네요!'
         else:
             examples = '중간에 그만 두셧군요.'
-    
-        url = f"https://api.neople.co.kr/df/servers/{servers[SERVER_NAME]}/characters?characterName={name}&apikey={API_KEY}"
-        response = requests.get(url)
-        player_info = response.json()
-    
-        char_lv = player_info['rows'][0]['level']
-        charname = player_info['rows'][0]['characterName']
-        charclass = player_info['rows'][0]['jobName']
+        F_label = st.radio('게임을 접게 된 이유가 무엇인가요?', ['다른 게임이 더 재미있어서', '퀘스트가 지루해서','직업이 재미없어서', '다른 서버에서 키우려고','점핑권이 없어서','게임 할 시간이 없어서','그 외 기타'])
+        label = {'다른 게임이 더 재미있어서':0,'퀘스트가 지루해서':1,'직업이 재미없어서':2,'다른 서버에서 키우려고':3, '점핑권이 없어서':4,'게임 할 시간이 없어서':5,'그 외 기타':6}
+        label=label[F_label]
         research = st.text_area(
-            f'{examples} 혹시 게임 중간에 마음에 안드는 구간이나 컨텐츠는 무엇이였나요? 혹은 재미없거나 흥미가 떨어진다고 생각하시는 부분은 어떤 것인가요?')
+            f'{examples} {charname}님 혹시 게임 중간에 마음에 안드는 구간이나 컨텐츠는 무엇이였나요? 혹은 재미없거나 흥미가 떨어진다고 생각하시는 부분은 어떤 것인가요?')
         if st.button('제출'):
             st.write('감사합니다!')
-            data.append([charservername,charname, charclass, char_lv, research])
+            data.append([charservername,charname, charclass, char_lv, research,label])
             df = pd.DataFrame(data)
             #df = pd.DataFrame(data, columns=['ch_id', 'class', 'LV', 'research'])
             print(df)
